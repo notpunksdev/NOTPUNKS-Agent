f00e459ba3dc084b0ec261841f3e4d145e0460d6
NOTPUNKS <notpunksdev@gmail.com>
2026-05-12 17:58:02 +0300

    chore: bump version to v0.11.13 (2026.5.12)

--- hermes_cli/__init__.py
+++ hermes_cli/__init__.py
@@ -11,5 +11,5 @@ Provides subcommands for:
 - notpunks cron          - Manage cron jobs
 """
 
-__version__ = "0.11.12"
-__release_date__ = "2026.5.9"
+__version__ = "0.11.13"
+__release_date__ = "2026.5.12"
